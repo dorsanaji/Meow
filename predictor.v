ef3180821c66a24e6a6af9ac23f5b02bb960b5fa
dorsanaji <79266118+dorsanaji@users.noreply.github.com>
2023-06-01 10:56:56 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,5 +1,13 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
+  reg state[1:0] = 2'b11;
 
-// Your code
-
+always@(posedge clk) begin
+     if (request)
+          prediction = state[1];
+     else if (result)
+          if (taken && state!=2'b11)
+                state = state+1;
+          else if (!taken && state!=2'b00)
+                 state = state-1;
+end
 endmodule
