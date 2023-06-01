3a4ce2af317d5d58bb669926f158204c95c18f91
dorsanaji <79266118+dorsanaji@users.noreply.github.com>
2023-06-01 11:05:50 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,18 +1,18 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
-reg state[1:0] = 2'b11;
+reg [1:0] state = 2'b11;
 
 
-   always@(posedge clk) begin
-        if (request) begin
-          prediction = state[1];
-         end
-        if (result) begin
-             if (taken && state!=2'b11) begin
-                 state = state+1;
+always@(posedge clk) begin
+if (request) begin
+prediction = state[1];
 end
-             if (!taken && state!=2'b00) begin
-              state = state-1;
+
+if (result) begin
+//todo
+if (taken && state!=2'b11) begin
+state = state+1;
+end
+if (!taken && state!=2'b00)begin
+state = state-1;
 end
 end
-end
-endmodule
